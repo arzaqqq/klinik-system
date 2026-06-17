9589cb59dd7bf1015d1be747e2f062d097b2230f
Arzaqqq <mmiftahularzaq01@gmail.com>
2026-06-18 02:40:27 +0700

    ADD  : Data seeder dan css tailwind

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,83 +1,49 @@
-import { prisma } from "../lib/prisma"
+import { PrismaClient } from "@prisma/client"
 import bcrypt from "bcryptjs"
 
+const prisma = new PrismaClient()
+
 async function main() {
-  // clear data (biar aman test ulang)
   await prisma.userRole.deleteMany()
   await prisma.user.deleteMany()
   await prisma.role.deleteMany()
 
-  // 1. CREATE ROLES
-  const superAdmin = await prisma.role.create({
-    data: { name: "Super Admin" }
-  })
+  const superAdmin = await prisma.role.create({ data: { name: "Super Admin" } })
+  const doctor = await prisma.role.create({ data: { name: "Doctor" } })
+  const nurse = await prisma.role.create({ data: { name: "Nurse" } })
+  const patient = await prisma.role.create({ data: { name: "Patient" } })
 
-  const doctor = await prisma.role.create({
-    data: { name: "Doctor" }
-  })
-
-  const nurse = await prisma.role.create({
-    data: { name: "Nurse" }
-  })
-
-  const patient = await prisma.role.create({
-    data: { name: "Patient" }
-  })
-
-  // 2. HASH PASSWORD DEFAULT
   const hash = await bcrypt.hash("123456", 10)
 
-  // 3. CREATE USERS
-
   const adminUser = await prisma.user.create({
-    data: {
-      username: "admin",
-      password: hash
-    }
+    data: { username: "admin", password: hash }
   })
 
   const doctorUser = await prisma.user.create({
-    data: {
-      username: "doctor",
-      password: hash
-    }
+    data: { username: "doctor", password: hash }
   })
 
   const nurseUser = await prisma.user.create({
-    data: {
-      username: "nurse",
-      password: hash
-    }
+    data: { username: "nurse", password: hash }
   })
 
   const patientUser = await prisma.user.create({
-    data: {
-      username: "patient",
-      password: hash
-    }
+    data: { username: "patient", password: hash }
   })
-
-  // 4. ASSIGN ROLES
 
   await prisma.userRole.createMany({
     data: [
       { userId: adminUser.id, roleId: superAdmin.id },
-
       { userId: doctorUser.id, roleId: doctor.id },
-
       { userId: nurseUser.id, roleId: nurse.id },
-
       { userId: patientUser.id, roleId: patient.id }
     ]
   })
 
-  console.log("Seed selesai ✔")
+  console.log("Seed sukses ✔")
 }
 
 main()
-  .then(() => prisma.$disconnect())
-  .catch(async (e) => {
-    console.error(e)
+  .finally(async () => {
     await prisma.$disconnect()
-    process.exit(1)
   })
